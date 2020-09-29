913113eb4251c588d37835fd8c8c040f1b712420
repl.it user <replituser@example.com>
2020-09-29 17:13:41 +0000

    Updated code to display a message with escape characters

--- 1-basics/1-output/multiline_message.py
+++ 1-basics/1-output/multiline_message.py
@@ -1,9 +1,2 @@
 # Display message to the standard output
-print("System Failure Imminent")
-print("system reboot has been initiated...")
-
-print ("...rebooting sensory system")
-print ("rebooting output motors")
-print ("rebooting hover engine")
-
-print ("system online")
+print("System Failure Imminent\nsystem reboot has been initiated...\n \n...rebooting sensory system \nrebooting output motors \nrebooting hover engine \n \n system online")
