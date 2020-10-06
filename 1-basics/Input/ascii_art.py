5abacffbfc1213523895e6ac22e9964321df74d8
SophiaAttrill1 <5attrs34@solent.ac.uk>
2020-10-06 10:32:57 +0000

    Added code to demonstrate the use of an if statement

--- 1-basics/Input/ascii_art.py
+++ 1-basics/Input/ascii_art.py
@@ -4,6 +4,7 @@ print("#        #")
 print("#        #")
 print("##########")
 
+<<<<<<< HEAD
 
 leftEye = input("Please enter a character for the left eye")
 rightEye = input("Please enter a character for the right eye")
@@ -14,3 +15,11 @@ print("# {}   {}  #".format(leftEye, rightEye))
 print("#   {}    #".format(mouth))
 print("##########")
 
+=======
+name = input("Please enter a character for the eye")
+
+print("##########")
+print("# {}   O #".format(name))
+print("#        #")
+print("##########")
+>>>>>>> origin/master
---
+++ 1-basics/simple_decision/if.py
@@ -0,0 +1,5 @@
+print("What kind of book are you reading?")
+genre = input()
+if genre == "adventure" or genre == "Adventure":
+ print("I like adventure books")
+print("Finished reading book!")
