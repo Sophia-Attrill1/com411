74b40f5ba91282c888a3c5634fade222c4990c96
repl.it user <replituser@example.com>
2020-10-08 18:18:22 +0000

    Added Or operators to our code so an input can have multiple outputs

---
+++ 1-basics/decisions/or_operator.py
@@ -0,0 +1,10 @@
+adventure = input("What type of adventure should I have?")
+
+if ((adventure == "short") or (adventure == "scary")):
+  print("Taking the scary route!")
+
+if ((adventure == "long") or (adventure == "safe")):
+  print("Taking the safe route!")
+
+else:
+  print("Not sure what route to take!")
